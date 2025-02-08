2eb31142200bcdb3a6388afdaff96d6c5bcb9bf4
zarruk16 <abdussalamzarruk@gmail.com>
2025-02-08 14:07:57 +0100

    fixing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { Link, NavLink, useLocation } from "react-router-dom";
-import { MdOutlineCancel } from "react-icons/md";
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import React, { useState } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import { MdOutlineCancel } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { links, bottomLinks } from "../data/dummy";
-import { useStateContext } from "../contexts/ContextProvider";
+import { links, bottomLinks } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
   const { currentColor, activeMenu, setActiveMenu, screenSize } =
@@ -12,9 +12,9 @@ const Sidebar = () => {
   const location = useLocation();
   const [activeBottomLink, setActiveBottomLink] = useState(null);
 
-  // Function to close the sidebar on small screens
+  // Close sidebar on small screens
   const handleCloseSideBar = () => {
-    if (screenSize <= 900 && activeMenu !== undefined) {
+    if (screenSize <= 900 && activeMenu) {
       setActiveMenu(false);
     }
   };
@@ -27,68 +27,62 @@ const Sidebar = () => {
 
   // CSS classes for active and normal links
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 flex flex-col justify-between">
+    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 flex flex-col justify-between'>
       {activeMenu && (
         <>
           {/* Sidebar Top Section */}
           <div>
-            <div className="flex justify-between items-center">
+            <div className='flex justify-between items-center'>
               <Link
-                to="/"
+                to='/'
                 onClick={handleCloseSideBar}
-                className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+                className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'
               >
                 <span style={{ color: currentColor }}>SKILLS PORTAL</span>
               </Link>
-              <TooltipComponent content="Menu" position="BottomCenter">
+              <TooltipComponent content='Menu' position='BottomCenter'>
                 <button
-                  type="button"
+                  type='button'
                   onClick={() => setActiveMenu(!activeMenu)}
                   style={{ color: currentColor }}
-                  className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+                  className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
                 >
                   <MdOutlineCancel />
                 </button>
               </TooltipComponent>
             </div>
-            <div className="mt-10">
+            <div className='mt-10'>
               {links.map((item, index) => (
                 <div key={item.name}>
-                  <p className="text-gray-400 capitalize dark:text-gray-400 m-3 mt-4 uppercase">
+                  <p className='text-gray-400 capitalize dark:text-gray-400 m-3 mt-4 uppercase'>
                     {item.name}
                   </p>
                   {item.links.map((link, linkIndex) => {
                     const isFirstLink = index === 0 && linkIndex === 0;
+                    const isActive =
+                      location.pathname === `/${link.name}` ||
+                      (isFirstLink && location.pathname === '/');
+
                     return (
                       <NavLink
                         to={`/${link.name}`}
                         key={link.name}
                         onClick={handleTopLinkClick}
-                        style={({ isActive }) => ({
-                          backgroundColor:
-                            isActive ||
-                            (isFirstLink && location.pathname === "/")
-                              ? currentColor
-                              : "",
-                        })}
-                        className={({ isActive }) =>
-                          isActive || (isFirstLink && location.pathname === "/")
-                            ? activeLink
-                            : normalLink
-                        }
+                        style={{
+                          backgroundColor: isActive ? currentColor : '',
+                        }}
+                        className={isActive ? activeLink : normalLink}
                       >
                         {React.cloneElement(link.icon, {
                           color:
-                            activeBottomLink === link.name
-                              ? "white"
-                              : "#007BFF",
+                            activeBottomLink === link.name ? 'white' : '#007BFF',
                         })}
-                        <span className="capitalize">{link.name}</span>
+                        <span className='capitalize'>{link.name}</span>
                       </NavLink>
                     );
                   })}
@@ -98,25 +92,25 @@ const Sidebar = () => {
           </div>
 
           {/* Sidebar Bottom Section */}
-          <div className="mt-auto">
-            <div className="dark:border-gray-700 pt-4">
+          <div className='mt-auto'>
+            <div className='dark:border-gray-700 pt-4'>
               {bottomLinks.map((link) => (
                 <NavLink
                   key={link.name}
                   to={`/${link.name}`}
-                  onClick={() => setActiveBottomLink(link.name)} // Track which bottom link was clicked
+                  onClick={() => setActiveBottomLink(link.name)}
                   className={`flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2 ${
-                    link.name === "logout"
-                      ? "text-red-600 hover:text-red-700"
+                    link.name === 'logout'
+                      ? 'text-red-600 hover:text-red-700'
                       : activeBottomLink === link.name
-                      ? "text-white bg-blue-500 dark:bg-blue-600"
-                      : "text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray"
+                      ? 'text-white bg-blue-500 dark:bg-blue-600'
+                      : 'text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray'
                   }`}
                 >
                   {React.cloneElement(link.icon, {
-                    color: activeBottomLink === link.name ? "white" : "#007BFF",
+                    color: activeBottomLink === link.name ? 'white' : '#007BFF',
                   })}
-                  <span className="capitalize">{link.name}</span>
+                  <span className='capitalize'>{link.name}</span>
                 </NavLink>
               ))}
             </div>
