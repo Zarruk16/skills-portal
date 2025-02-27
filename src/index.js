fef9661333a9c928c682b0fec8481b5e994441d2
zarruk16 <abdussalamzarruk@gmail.com>
2025-02-27 13:31:23 +0100

    fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
+root.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
